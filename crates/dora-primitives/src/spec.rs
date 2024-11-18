73dfd654f3398d0c7c7e804cf8e98598b21b069a
xduo <xduo@xduodeMacBook-Air.local>
2024-11-18 20:47:08 +0800

    revole specid def based on revm specid

--- crates/dora-primitives/src/spec.rs
+++ crates/dora-primitives/src/spec.rs
@@ -1,4 +1,7 @@
+use revm_primitives::SpecId as rSpecId;
 use serde::{Deserialize, Serialize};
+
+type SpecId = rSpecId;
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Hash)]
 pub enum SpecName {
@@ -24,8 +27,34 @@ pub enum SpecName {
     Shanghai,
     Cancun,
     Prague,
-    PragueEOF,
     Osaka, // SKIPPED
     #[serde(other)]
     Unknown,
+}
+
+impl SpecName {
+    pub fn to_spec_id(&self) -> SpecId {
+        match self {
+            Self::Frontier => SpecId::FRONTIER,
+            Self::Homestead | Self::FrontierToHomesteadAt5 => SpecId::HOMESTEAD,
+            Self::EIP150 | Self::HomesteadToDaoAt5 | Self::HomesteadToEIP150At5 => {
+                SpecId::TANGERINE
+            }
+            Self::EIP158 => SpecId::SPURIOUS_DRAGON,
+            Self::Byzantium | Self::EIP158ToByzantiumAt5 => SpecId::BYZANTIUM,
+            Self::ConstantinopleFix | Self::ByzantiumToConstantinopleFixAt5 => SpecId::PETERSBURG,
+            Self::Istanbul => SpecId::ISTANBUL,
+            Self::Berlin => SpecId::BERLIN,
+            Self::London | Self::BerlinToLondonAt5 => SpecId::LONDON,
+            Self::Paris | Self::Merge => SpecId::MERGE,
+            Self::Shanghai => SpecId::SHANGHAI,
+            Self::Cancun => SpecId::CANCUN,
+            Self::Prague => SpecId::PRAGUE,
+            Self::ByzantiumToConstantinopleAt5 | Self::Constantinople => {
+                panic!("Overridden with PETERSBURG")
+            }
+            Self::Osaka => panic!("Osaka is not implemented"),
+            Self::Unknown => panic!("Unknown spec"),
+        }
+    }
 }
