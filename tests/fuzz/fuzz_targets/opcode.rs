9bbc381e3d883d8366608832a2029f021f58fb06
Gabriel <timi@pharoslabs.xyz>
2024-11-25 21:32:27 +0800

    fix: fuzz target call with spec id (#63)
    Signed-off-by: Gabriel <timi@pharoslabs.xyz>

--- tests/fuzz/fuzz_targets/opcode.rs
+++ tests/fuzz/fuzz_targets/opcode.rs
@@ -1,9 +1,10 @@
 #![no_main]
 
 use dora::run_evm_bytecode_with_calldata;
+use dora_primitives::spec::SpecId;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     let program = hex::encode(data);
-    let _ = run_evm_bytecode_with_calldata(&program, "", 999_999);
+    let _ = run_evm_bytecode_with_calldata(&program, "", 999_999, SpecId::CANCUN);
 });
