eaf5da3698a50b8c8cfed88bf2a38aae45ecf30b
timi-y <timi@pharoslabs.xyz>
2025-03-26 10:48:28 +0800

    feat: reexport more coversion APIs and add doc tests
    Signed-off-by: timi-y <timi@pharoslabs.xyz>

--- crates/dora-compiler/src/conversion/mod.rs
+++ crates/dora-compiler/src/conversion/mod.rs
@@ -4,3 +4,7 @@ pub mod walker;
 
 #[cfg(test)]
 mod tests;
+
+pub use builder::OpBuilder;
+pub use rewriter::{DeferredRewriter, Replacer, Rewriter, move_all_ops_before_op, replace_op};
+pub use walker::{WalkFn, walk_operation};
